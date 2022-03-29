3c4004067d036e3e114bb00d52d164b804d3f2c9
nhthuong2208 <73584596+nhthuong2208@users.noreply.github.com>
2022-03-29 15:40:38 +0700

    add something

--- src/components/header.js
+++ src/components/header.js
@@ -26,5 +26,56 @@ const LoggedOutView = React.memo(props => {
         );
     }
     return null;
+});
+
+const LoggedInView = React.memo(props => {
+    if (props.currentUser) {
+        return (
+            <ul className="nav navbar-nav pull-xs-right">
+                <li className="nav-item">
+                    <Link to="/" className="nav-link">
+                        Home
+                    </Link>
+                </li>
+
+                <li className="nav-item">
+                    <Link to="/editor" className="nav-link">
+                        <i className="ion-compose"></i>&nbsp;New Post
+                    </Link>
+                </li>
+
+                <li className="nav-item">
+                    <Link to="/setting" className="nav-link">
+                        <i className="ion-gear-a"></i>&nbsp;Settings
+                    </Link>
+                </li>
+
+                <li className="nav-item">
+                    <Link to="/account" className="nav-link">
+                        <img src="https://static.productionready.io/images/smiley-cyrus.jpg" className="user-pic"/>
+                        Thuong
+                    </Link>
+                </li>
+            </ul>
+        );
+    }
+    return null;
 })
 
+const Header = () => {
+    return (
+        <nav className="navbar navbar-light">
+            <div className="container">
+                <Link to="/" className="navbar-brand">
+                    Conduit
+                </Link>
+
+                <LoggedOutView currentUser={null} />
+                <LoggedInView currentUser={null} />
+            </div>
+
+        </nav>
+    )
+}
+
+export default Header
